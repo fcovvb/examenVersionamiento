9dbeb44c5ede11041a0a683ddd85d60233311397
fcovvb <fcovvb@gmail.com>
2017-12-27 20:42:45 -0300

    Se crea metodo Consulta2 en clase Registro. Este método lista los registros dónde los empleados pertenezcan al departamento de Redes.

--- src/Modelo/Registro.java
+++ src/Modelo/Registro.java
@@ -36,7 +36,7 @@ public class Registro {
     public boolean agregar(int codigo, String rut, String nombre, String apellido,  int celular, String correo, int sueldobruto, String est_civil, String nom_depto){
         if( valida_datos(codigo, rut, nombre, apellido, celular, sueldobruto) ) {
             //Se arma la consulta
-            String q=" INSERT INTO taller3.pelicula(codigo,rut,nombre,apellido,celular,email, sueldo_bruto, est_civil, nom_depto) "
+            String q=" INSERT INTO examenversionamiento.empleados(codigo,rut,nombre,apellido,celular,email, sueldo_bruto, est_civil, nom_depto) "
                     + "VALUES ( '" + codigo + "','" + rut + "','"+ nombre +"','" + apellido +"','" + celular +"','" + correo +"','" + sueldobruto +"','" + est_civil +"','" + nom_depto +"');";
             //se ejecuta la consulta
             try {
@@ -164,5 +164,42 @@ public class Registro {
         return false;}
     }
     
+     public DefaultTableModel Consulta2(){
+     DefaultTableModel tablemodel = new DefaultTableModel();
+     int registros = 0;
+     String[] columNames = {"Código","RUT","Nombre","Apellido","Celular","Correo","Sueldo Bruto", "Est Civil","Depto"};
+     try{
+        PreparedStatement pstm = conectara.conectar().prepareStatement( "SELECT count(*) as total FROM examenversionamiento.empleados WHERE nom_depto = 'Redes';"); //7 = romance
+        ResultSet res = pstm.executeQuery();
+        res.next();
+        registros = res.getInt("total");
+        res.close();
+     }catch(SQLException e){
+        System.err.println( e.getMessage() );
+     }
+     Object[][] data = new String[registros][9];
+     try{
+        PreparedStatement pstm = conectara.conectar().prepareStatement("SELECT * FROM examenversionamiento.empleados WHERE nom_depto = 'Redes';");
+        ResultSet res = pstm.executeQuery();
+        int i=0;
+        while(res.next()){
+               data[i][0] = res.getString( "codigo" );
+                data[i][1] = res.getString( "rut" );
+                data[i][2] = res.getString("nombre");
+                data[i][3] = res.getString( "apellido" );
+                data[i][4] = res.getString( "celular" );
+                data[i][5] = res.getString( "email" );
+                data[i][6] = res.getString( "sueldo_bruto" );
+                data[i][7] = res.getString( "est_civil" );
+                data[i][8] = res.getString( "nom_depto" );
+           i++;
+        }
+        res.close();
+        tablemodel.setDataVector(data, columNames );
+        }catch(SQLException e){
+           System.err.println( e.getMessage() );
+       }
+       return tablemodel;
+   }
     
 }
