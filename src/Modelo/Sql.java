652c054f5b8a4e136069d08bb77de670f50358b5
fcovvb <fcovvb@gmail.com>
2017-12-27 19:31:32 -0300

    Se escribe el código de conexión para la clase SQL. Se incluye base de datos url, usuario y contraseña para realizar consultas. Se agrega código para acceder al driver jdbc.

---
+++ src/Modelo/Sql.java
@@ -0,0 +1,18 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Sql {
+    Connection conectar=null;
+    public Connection conectar(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection(""
+                    + "jdbc:mysql://superbetmatch.mysql.database.azure.com:3306/examenversionamiento?useSSL=true&requireSSL=false","fcovvb@superbetmatch","Tabla22#");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return conectar;
+    }
+}
